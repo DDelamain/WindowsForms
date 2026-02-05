f2981363f56bafe354ab1c0475f31c59f9e229c8
dando <danila.dor.1125@mail.ru>
2026-02-06 00:26:15 +0500

    Font load from settings DONE

--- Clock/FontDialog.cs
+++ Clock/FontDialog.cs
@@ -76,7 +76,8 @@ namespace Clock
 				this.parent.Location.X - this.Width/2, 
 				this.parent.Location.Y + 100
 				);
-			LoadFonts();
+			//LoadFonts();
+			//ApplyFontExample(FontFile);
 		}
 
 		private void buttonOK_Click(object sender, EventArgs e)
@@ -84,22 +85,22 @@ namespace Clock
 			this.Font = labelExample.Font;
 			this.FontFile = fonts[comboBoxFonts.SelectedItem.ToString()];
 		}
-		void ApplyFontExample()
+		public Font ApplyFontExample(string filename)
 		{
 			if (pfc != null) pfc.Dispose();
 			pfc = new PrivateFontCollection();
-			pfc.AddFontFile(fonts[comboBoxFonts.SelectedItem.ToString()]);
-			labelExample.Font = new Font(pfc.Families[0], (float)numericUpDownFontSize.Value);	
+			pfc.AddFontFile(filename);
+			return labelExample.Font = new Font(pfc.Families[0], (float)numericUpDownFontSize.Value);	
 
 		}
 		private void comboBoxFonts_SelectedIndexChanged(object sender, EventArgs e)
 		{
-			ApplyFontExample();
+			ApplyFontExample(fonts[comboBoxFonts.SelectedItem.ToString()]);
 		}
 
 		private void numericUpDownFontSize_ValueChanged(object sender, EventArgs e)
 		{
-			ApplyFontExample();
+			ApplyFontExample(fonts[comboBoxFonts.SelectedItem.ToString()]);
 		}
 	}
 }
